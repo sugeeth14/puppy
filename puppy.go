ba175caa2375c6a9e9e2e031dcfc7d85f0c70f2e
sugeeth14 <raghava.sugeeth@gmail.com>
2023-06-02 00:08:21 -0600

    Added loud bark

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/sugeeth14/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!!!"
+}
+
+func BarkLoudly() string {
+	return dog.WhenGrownUp("Loud BARK!!!!")
 }
